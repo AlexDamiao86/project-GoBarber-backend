c1e5743d5b48599d85770a32395d52ba49730081
AlexDamiao86 <alex_maiabr@hotmail.com>
2021-03-29 15:45:59 -0300

    Lista prestadores e testa

---
+++ src/modules/appointments/services/ListProvidersService.spec.ts
@@ -0,0 +1,41 @@
+import FakeUsersRepository from '@modules/users/repositories/fakes/FakeUsersRepository';
+import ListProvidersService from './ListProvidersService';
+
+let fakeUsersRepository: FakeUsersRepository;
+let listProviders: ListProvidersService;
+
+// Describe é uma forma de organizar os testes, colocando um cabeçalho
+describe('ListProviders', () => {
+  beforeEach(() => {
+    fakeUsersRepository = new FakeUsersRepository();
+
+    listProviders = new ListProvidersService(fakeUsersRepository);
+  });
+
+  it('should be able to list providers', async () => {
+    // Cria um novo usuario
+    const user1 = await fakeUsersRepository.create({
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password: '123456',
+    });
+
+    const user2 = await fakeUsersRepository.create({
+      name: 'John Kennan',
+      email: 'johnk@example.com',
+      password: '123456',
+    });
+
+    const loggedUser = await fakeUsersRepository.create({
+      name: 'Johnny Mac',
+      email: 'johnnymac@example.com',
+      password: '123456',
+    });
+
+    const providers = await listProviders.execute({
+      user_id: loggedUser.id,
+    });
+
+    expect(providers).toEqual([user1, user2]);
+  });
+});
